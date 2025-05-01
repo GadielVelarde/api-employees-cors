af0556982ca9c57f74ddb7c4dde3821b07ef3dcc
GadielVelarde <113541682+GadielVelarde@users.noreply.github.com>
2025-05-01 12:20:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-host_name = "172.31.22.204" # IPv4 privada de "MV Bases de Datos"
+host_name = "172.31.20.19" # IPv4 privada de "MV Bases de Datos"
 port_number = "8005"
 user_name = "root"
 password_db = "utec"
